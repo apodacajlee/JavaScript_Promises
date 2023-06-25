00ece50d7840473013a51ebe39f572ab5d6bdb40
Joshua Apodaca <apodacajlee@gmail.com>
2023-06-24 21:46:01 -0500

    ready to submit

--- app.js
+++ app.js
@@ -15,10 +15,35 @@ function getList() {
   });
 }
 
+// Exercise 1: Select elements
+const error = document.getElementById("error");
+const list = document.getElementById("list");
+
+let promise = getList();
+
 // TODO: Handle the resolved or rejected states of the promise
+promise.then(
+  function (result) {
+    // TODO: If the promise resolves with the list of hobbits
+    // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+    for (let i of result) {
+      const li = document.createElement("li");
+      li.textContent = i;
+      list.appendChild(li);
+    }
+  }
+).catch(
+  // TODO: If the promise rejects with the failure object
+  // Display the failure message in the paragraph element with id="error" (check index.html file)
+  function (result) {
+    error.textContent = result.message;
+  }
+);
 
-// TODO: If the promise resolves with the list of hobbits
-// Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
 
-// TODO: If the promise rejects with the failure object
-// Display the failure message in the paragraph element with id="error" (check index.html file)
+
+
+
+
+
+
